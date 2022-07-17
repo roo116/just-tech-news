2c05571f59c7bc9165607d0b2162044f63a85075
roo116 <roo116@nc.rr.com>
2022-07-17 15:11:54 -0400

    pushing up work so I can get to it on my PC

--- config/connection.js
+++ config/connection.js
@@ -3,10 +3,18 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connection to our db
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+const sequelize = new Sequelize('just_tech_news_db', 'root', 'root', {
   host: 'localhost',
   dialect: 'mysql',
+  pool: {
+    max: 9,
+    min: 0,
+    idle: 10000
+  },
   port: 3306
 });
+
+
 
 module.exports = sequelize;
