efda8f749e1fd7f22b0f6227e1be312d9f085698
roo116 <roo116@nc.rr.com>
2022-07-18 17:06:30 -0400

    fixed merge issues I think

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-route')
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes)
 
 module.exports = router;
